c0456a3cf7e940afa6d0d5bf20f2f1316682e659
Nick Menzhuber <nickmenzhuber@gmail.com>
2023-01-28 10:46:50 -0600

    awesome update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 export default function Header() {
     return (
         <div className="header">
-            <h1>This is a header</h1>
+            <h1>This awesome header</h1>
             <Navigation />
         </div>
     )
