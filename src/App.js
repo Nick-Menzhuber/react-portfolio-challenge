323b4cc1818d445b415aeb9d5b5424acac6f1865
Nick Menzhuber <nickmenzhuber@gmail.com>
2023-01-28 10:34:43 -0600

    added component structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Resume from './components/Resume';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Navigation from './Navigation';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1>This is a header</h1>
+            <Navigation />
+        </div>
+    )
+}
