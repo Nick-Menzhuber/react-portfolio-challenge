d5f5b8e9697bd0a3795dadf39415d734ce040459
Nick Menzhuber <nickmenzhuber@gmail.com>
2023-02-03 01:12:08 -0600

    walks back previous commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,7 @@
 import '../styles/Navigation.css';
 
 export default function Navigation() {
-    document.getElementById('contact').style = 'display : none';
-    document.getElementById('portfolio').style = 'display : none';
-    document.getElementById('resume').style = 'display : none';
-    document.getElementById('about').style = 'display : block';
+ 
     function setContact() {
         document.getElementById('about').style = 'display : none';
         document.getElementById('portfolio').style = 'display : none';
